6490355a92c5a49cbaaf446771558ce2cdc04812
RandieSasongko <122023576+RandieSasongko123@users.noreply.github.com>
2025-10-18 18:59:35 +0700

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -121,7 +121,7 @@ const About = () => {
           animate={{ x: 0, opacity: 1 }}
           transition={{ delay: 0.4, duration: 0.8 }}
         >
-          <div className="w-48 md:w-64 lg:w-80 border-b-4 border-[#FF6500] rounded-lg overflow-hidden">
+          <div className="w-48 md:w-64 lg:w-80 border-b-4 border-[#FF6500] rounded-lg overflow-hidden box-profile">
             <img
               src={profilePhoto}
               alt="photo_profile"
