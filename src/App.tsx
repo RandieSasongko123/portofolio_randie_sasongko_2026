51e9a49771f91cd3c20b6ba98cc1b2b9b0c33eef
RandieSasongko <122023576+RandieSasongko123@users.noreply.github.com>
2025-10-18 12:28:13 +0700

    first commit
    Kerangka Kerja React + Vite + Tailwind CSS + ( Flowbite )

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// import './App.css'
+import { Button } from "flowbite-react";
+
+function App() {
+  return (
+    <>
+      <Button>Click me</Button>;
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
